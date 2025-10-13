ce0c523a024eff5e1284289c9b91ea12fdd71d9b
Ranjit Kumar Singh <ranjitksingh.079@gmail.com>
2025-10-13 22:34:04 +0530

    Signup and login page frontend and backend completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import path from "path";
 import cookieParser from "cookie-parser";
+import cors from "cors"
+
 
 import {connectDB} from "./lib/db.js"
 // Load .env explicitly from Backend root
@@ -13,6 +15,11 @@ import messageRoutes from "./routes/message.route.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true,
+}));
+
 
 app.get("/",(req,res)=>{
   res.send("Blink Backend API is running Successfully!")
---
+++ Frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loginPage = () => {
+  return (
+    <div>
+      Login Page
+    </div>
+  )
+}
+
+export default loginPage
